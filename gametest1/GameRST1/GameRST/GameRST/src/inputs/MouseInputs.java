41a6069ca2a15026331c326a4171d7fcb8cf1f1b
SeanMilloy23 <94382542+SeanMilloy23@users.noreply.github.com>
2023-06-14 23:06:56 -0400

    Add files via upload

---
+++ gametest1/GameRST1/GameRST/GameRST/src/inputs/MouseInputs.java
@@ -0,0 +1,59 @@
+package inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import main.GamePanel;
+
+public class MouseInputs implements MouseListener, MouseMotionListener{
+
+	private GamePanel gamePanel;
+	public MouseInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		gamePanel.setRectPos(arg0.getX(),arg0.getY());
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("mouse clicked");
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
